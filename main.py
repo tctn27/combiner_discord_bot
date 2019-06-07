be4e0d9f7e8453d59baff94815ce2a0dde15462f
Tom Dunbavan <tomcatthenommer@gmail.com>
2019-06-08 00:26:40 +1000

    fixed it talking to itself and not pinging devs on errors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import time
 import discord
 from discord.ext import commands
 
@@ -21,23 +22,18 @@ def combine(first_word, second_word):
             else:
                 break
 
-    temp = ""
-    first_flag = False
-    second_flag = False
-    for i in second_word:
-        if second_flag:
-            temp += i
-        elif i not in vowels:
-            first_flag = True
-        elif first_flag:
-            temp += i
-            second_flag = True
+    i = -1
+    for l in second_word:
+        i += 1
+        if l in vowels:
+            out_word += second_word[i:]
+            return out_word
 
     return out_word
 
 def give_eligible_words(sentence):
-    words = sentence.content.split(" ")
-    long = 3
+    words = sentence.content.lower().split(" ")
+    long = 2
     eligible_words = []
     was_long = False
     buffer = ""
@@ -66,17 +62,20 @@ async def on_message(message):
                 await message.channel.send("https://i.kym-cdn.com/entries/icons/original/000/026/008/Screen_Shot_2018-04-25_at_12.24.22_PM.png")
             else:
                 words = give_eligible_words(message)
-                if len(message.content.split(" ")) == 2:
+                if len(message.content.split(" ")) == 2 and len(words) > 0:
                     set_of_two = words[random.randint(0, len(words) - 1)]
                     combined = combine(set_of_two[0], set_of_two[1])
                     print(combined)
-                    await message.channel.send(combined)
-                elif len(words) > 0 and random.randint(0, 10) == 0:
+                    await message.channel.send("*" + combined + "*")
+                elif len(words) > 0 and random.randint(0, 0) == 0:
                     set_of_two = words[random.randint(0, len(words) - 1)]
                     combined = combine(set_of_two[0], set_of_two[1])
                     print(combined)
-                    await message.channel.send(combined)
-        except ValueError:
+                    await message.channel.send("*" + combined + "*")
+        except ValueError as e:
+            with open("logs/" + str(time.time()) + ".log", "w+") as f:
+                f.write(str(time.time()) + "\n")
+                f.write(str(e))
             await message.channel.send("<@227336569881624576> <@191357391453945856> error")
 
 client.run(TOKEN)
