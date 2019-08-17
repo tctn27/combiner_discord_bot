d22ec7cd5b51fb8572ec14ceb1d6b94ce9bfbff0
Tom Dunbavan <tomcatthenommer@gmail.com>
2019-08-18 00:41:28 +1000

    emails all the time

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from discord.ext import commands
 from os import listdir
 import os
 
-with open("token", "r") as f:
-    TOKEN = f.read()
+with open("token", "r") as file:
+    TOKEN = file.read()
 
 client = commands.Bot(command_prefix="~")
 
@@ -84,17 +84,18 @@ def give_eligible_words(sentence):
 
 
 def store_meme(message):
-    with open(".\\memes\\" + str(time.time()), "a+") as f:
-        f.write(message)
+    with open(".\\memes\\" + str(time.time()), "a+") as storage_file:
+        storage_file.write(message)
 
 
 def get_meme():
     file_list = listdir(".\\memes\\")
-    file = file_list[random.randint(0, len(file_list) - 1)]
-    with open("memes/" + file, "r+") as f:
-        return str(f.read())
+    fetch_file = file_list[random.randint(0, len(file_list) - 1)]
+    with open("memes/" + fetch_file, "r+") as fetch_file:
+        return str(fetch_file.read())
 
 
+# noinspection PyArgumentList
 def uwuified(message=str):
     message = message.lower()
     message = message.replace("er ", "a ")
@@ -110,7 +111,7 @@ def uwuified(message=str):
 
 @client.event
 async def on_ready():
-    print("Bot online")
+    email_devs.online_email()
 
 
 @client.event
@@ -119,7 +120,9 @@ async def on_message(message):
         if message.channel not in blacklist:
             try:
                 if message.content.startswith("~uwu") or random.randint(0, 100) <= 9:
-                    await message.channel.send(uwuified(message.content.split("~uwu").strip()))
+                    await message.channel.send(uwuified(message.content.split("~uwu")[1].strip()))
+                elif True:
+                    await message.channel.send(uwuified(message.content.split("~uwu")[1].strip()))
                 elif message.content.startswith("~update") and message.channel.id == 317211750602768384:
                     await message.channel.send("Update inbound, shutting down momentarily")
                     os.system("update")
@@ -134,8 +137,9 @@ async def on_message(message):
                                                "ping: test the latency of Beems\n"
                                                "~uwu [message]: uwuifies the message\n"
                                                "    Also has a 1/100 chance of happening on any message\n"
-                                               "Beems also has a 1/10 chance of combining 2 words in any given sentence\n"
-                                                "~link: shows the link to add Beems to your other servers\n"
+                                               "Beems also has a 1/10 chance of combining 2 words in any given "
+                                               "sentence\n "
+                                               "~link: shows the link to add Beems to your other servers\n"
                                                "\n"
                                                "**Memes**\n"
                                                "\n"
@@ -143,13 +147,19 @@ async def on_message(message):
                                                "modok\n"
                                                "x")
                 elif message.content.startswith("~link"):
-                    await message.channel.send("Please follow this link to add Beems to your servers:\nhttps://discordapp.com/oauth2/authorize?client_id=585050654330847232&scope=bot&permissions=8")
+                    await message.channel.send("Please follow this link to add Beems to your "
+                                               "servers:\nhttps://discordapp.com/oauth2/authorize?client_id"
+                                               "=585050654330847232&scope=bot&permissions=8")
                 elif message.content == "e" or message.content == "E":
-                    await message.channel.send("https://i.kym-cdn.com/entries/icons/original/000/026/008/Screen_Shot_2018-04-25_at_12.24.22_PM.png")
+                    await message.channel.send("https://i.kym-cdn.com/entries/icons/original/000/026/008"
+                                               "/Screen_Shot_2018-04-25_at_12.24.22_PM.png")
                 elif "modok" in message.content or "MODOK" in message.content or "M.O.D.O.K." in message.content:
-                    await message.channel.send("https://vignette.wikia.nocookie.net/assistme/images/9/92/Modok.png/revision/latest?cb=20120710014024")
+                    await message.channel.send("https://vignette.wikia.nocookie.net/assistme/images/9/92/Modok.png"
+                                               "/revision/latest?cb=20120710014024")
                 elif message.content == "x" or message.content == "X":
-                    await message.channel.send("https://i.kym-cdn.com/entries/icons/original/000/023/021/e02e5ffb5f980cd8262cf7f0ae00a4a9_press-x-to-doubt-memes-memesuper-la-noire-doubt-meme_419-238.png")
+                    await message.channel.send("https://i.kym-cdn.com/entries/icons/original/000/023/021"
+                                               "/e02e5ffb5f980cd8262cf7f0ae00a4a9_press-x-to-doubt-memes-memesuper-la"
+                                               "-noire-doubt-meme_419-238.png")
                 elif message.content == "ping":
                     await message.channel.send("pong")
                 elif random.randint(0, 100) == 0:
@@ -172,9 +182,9 @@ async def on_message(message):
                         combined = combine(set_of_two[0], set_of_two[1])
                         await message.channel.send("*" + combined + "*")
             except Exception as e:
-                with open("logs/" + str(time.time()) + ".log", "w+") as f:
-                    f.write(str(time.time()) + "\n")
-                    f.write(str(e))
+                with open("logs/" + str(time.time()) + ".log", "w+") as log_file:
+                    log_file.write(str(time.time()) + "\n")
+                    log_file.write(str(e))
                 email_devs.error_email(e)
                 await message.channel.send("<@227336569881624576> <@191357391453945856> error")
 
